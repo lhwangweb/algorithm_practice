913541af6896ab6291494edd6d44e40505bf7810
hank <lhwang.web@gmail.com>
2022-05-19 20:17:17 +0800

    加入 Merge Sort 練習

---
+++ merge_sort.py
@@ -0,0 +1,212 @@
+"""
+Merge Sort 練習
+"""
+import copy
+import math
+import random
+import unittest
+
+#
+# 比較用函數
+#
+def be_less(a, b):
+    """ 比較規則 func, 當 a<b 時回傳 True"""
+    return True if a < b else False
+
+
+def be_less_equal(a, b):
+    """ 比較規則 func, 當 a<=b 時回傳 True"""
+    return True if a <= b else False
+
+
+def be_bigger(a, b):
+    """ 比較規則 func, 當 a>b 時回傳 True"""
+    return True if a > b else False
+
+
+def be_bigger_equal(a, b):
+    """ 比較規則 func, 當 a>=b 時回傳 True"""
+    return True if a >= b else False
+
+
+def merge1(data, compare_funtion):
+    """
+    合併排序
+    每次 D&C 的子任務都使用新的記憶體空間，比較費空間，但是 Code 相對比較單純
+
+    :param data: List, 要排序的 list
+    :param compare_rule_function: Func, 自訂的比較規則
+        根據 func(a,b) 行為來決定 ASC, DESC
+         - 當 a>b 時如果回傳 True -> DESC
+         - 反之，當 a<b 時 True -> ASC
+    :return: list, 本階段完成的排序結果
+    """
+    length = len(data)
+    if length == 0:
+        return data
+    elif length == 1:
+        return data
+    elif length == 2:
+        if not compare_funtion(data[0], data[1]):
+            data[0], data[1] = data[1], data[0]
+        return data
+
+    # 走到這裡時，data 應該至少有 3 個 Elem
+
+    # Divide - 二分切割
+    middle_idx = length//2
+
+    # 左半部
+    left_part = data[0:middle_idx]
+    # 右半部
+    right_part = data[middle_idx:]
+
+    # 1.1 Conquer - 繼續送進子任務
+    left_part = merge1(left_part, compare_funtion)
+    right_part = merge1(right_part, compare_funtion)
+
+    # 2. 拿到結果，此時左右兩個部分應該已經是個別排序好的結果，現在準備合併
+    left_length = len(left_part)
+    right_length = len(right_part)
+
+    # 新 list (合併用)
+    merged_result_list = []
+
+    # 用於遍歷 left_part 的 index
+    left_idx = 0
+    # 用於遍歷 right_part 的 index
+    right_idx = 0
+    while True:
+        # 不停 loop 遍歷 left_part 與 right_part，每一輪工作如下：
+        #   (1) 提取 Element 做比較： left_part 在 left_idx 的 Elem vs. 、右兩邊的 Elem
+        #   (2) 符合條件(ASC 時，看誰小，反之亦然)者，送入 list
+        #   (3) 之後那一邊的 index +=1 (例如： 如果是 left_part[left_idx] 送入 list，則 left_idx +=1)
+        if left_idx == left_length or right_idx == right_length:
+            # 代表其中一個  已經走完了，所以先結束
+            break
+
+        if compare_funtion(left_part[left_idx], right_part[right_idx]):
+            merged_result_list.append(left_part[left_idx])
+            left_idx += 1
+        else:
+            merged_result_list.append(right_part[right_idx])
+            right_idx += 1
+
+    # left_part right_part 長度、分佈都可能不同，有可能其中一個排完了，另一個還剩一截。
+    # 範例 (假設排 ASC)
+    #   left_part  [1, 3]
+    #   right_part [2, 6, 10]
+    #  以這兩個範例， 結束 while loop 時，狀態如下
+    #    merged_result_list 為 [1, 2, 3]
+    #    left_idx 2
+    #    right_idx 1  -> 所以 right_part 還剩 [6, 10] 還沒安排
+    #
+    # 此時也很簡單，就是把剩下的 [6, 10] 附加到 merged_result_list 右邊即可
+    # 以排 ASC 為例，因為已經排好的 merged_result_list 一定是相對比較小的 Elements，而剩下的一定是相對比較大的，而且本身就是排序好的，因此直接 append 即可
+
+    # 由於上面的 loop 最後有 += 1, 因此結束時會比最後一個 index 多 1，剛好等於 length 的長度，所以此處 condition 這樣寫
+    if left_idx < left_length:
+        # right_part 已經全部合併到新的list 而 left_part 還有剩，還沒結束的那部分，直接附加到 list 後面
+        merged_result_list += left_part[left_idx:]
+    if right_idx < right_length:
+        # left_part 已經全部合併到新的 list 而 right_part 還有剩， 還沒結束的那部分，直接附加到 list 後面
+        merged_result_list += right_part[right_idx:]
+
+    return merged_result_list
+
+class TestMethod(unittest.TestCase):
+    samples = []
+
+    def TestUtil_for2(self, data, test_func, compare_func, order):
+        """ 測試 in-place 類型的 快速排序 function """
+        print("  原本: %s" % str(data))
+        test_func(data, compare_func)
+        if order == "asc":
+            self.assertEqual(data, sorted(data, reverse=False))
+            print("  ASC 結果 %s\n" % (data,))
+        else:
+            self.assertEqual(data, sorted(data, reverse=True))
+            print("  DESC 結果 %s\n" % (data,))
+
+    def TestUtil_for1(self, data, test_func, compare_func, order):
+        """ 測試非 in-place 類型的 快速排序 function """
+        print("  原本: %s" % str(data))
+        result = test_func(data, compare_func)
+        if order == "asc":
+            self.assertEqual(result, sorted(data, reverse=False))
+            print("  ASC 結果 %s\n" % (result,))
+        else:
+            self.assertEqual(result, sorted(data, reverse=True))
+            print("  DESC 結果 %s\n" % (result,))
+
+    def setUp(self):
+        """ 產生測試案例 list """
+        N1 = random.randint(6, 16)
+        data_rd1 = [i for i in range(0, N1)]
+        random.shuffle(data_rd1)
+        N2 = random.randint(11, 27)
+        data_rd2 = [i for i in range(random.randint(1, 5), N2)]
+        data_rd2 += data_rd2
+        random.shuffle(data_rd2)
+        data_rd3 = [i for i in range(0, random.randint(7, 20))] + [6, 6, 6] + [7, 7, 7]
+        random.shuffle(data_rd3)
+        data_rd4_tmp1 = [i for i in range(random.randint(0, 10), random.randint(15, 20))]
+        data_rd4_tmp2 = [i for i in range(random.randint(0, 7), random.randint(12, 26))]
+        data_rd4 = data_rd4_tmp1 + data_rd4_tmp2
+        random.shuffle(data_rd4)
+        data_rd4.pop(random.randint(0, len(data_rd4) - 2))
+        self.samples = [
+            {"name": "iTHome Case", "list": [8, 6, 1, 10, 5, 3, 9, 2, 7, 4]},
+            {"name": "簡單序列 (奇數個)", "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]},
+            {"name": "簡單序列 (偶數個)", "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+            {"name": "零", "list": []},
+            {"name": "一個", "list": [4, ]},
+            {"name": "兩個", "list": [4, 5]},
+            {"name": "三個", "list": [4, 7, 9]},
+            {"name": "四個", "list": [4, 9, 8, 3]},
+            {"name": "交錯 (奇數個)", "list": [11, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6]},
+            {"name": "交錯 (偶數個)", "list": [1, 10, 2, 9, 3, 8, 4, 7, 5, 6]},
+            {"name": "隨機不重複 0 ~ %s 打亂" % N1, "list": data_rd1},
+            {"name": "固定帶重複 elem", "list": [6, 3, 8, 6, 4, 6, 9, 3, 3, 3, 6, 9, 7, 5]},
+            {"name": "隨機 兩倍重複  0~ %s" % N2, "list": data_rd2},
+            {"name": "隨機 加一些相同 elem 進去", "list": data_rd3},
+            {"name": "隨機 多加一些 elem 進去", "list": data_rd4},
+        ]
+
+    def test_merge1(self):
+        """ 測試 merge1 """
+        num = 1
+        for sample in self.samples:
+            # 每一個案例都測過以下八種情境排列組合
+            print("--- merge 1 第 %s 組測試案例 ---" % num)
+            print(sample["name"])
+            print(" 測 < ")
+            data = list(copy.deepcopy(sample["list"]))
+            self.TestUtil_for1(data=data, test_func=merge1, compare_func=be_less, order="asc")
+            print(" 測 <= ")
+            data = list(copy.deepcopy(sample["list"]))
+            self.TestUtil_for1(data=data, test_func=merge1, compare_func=be_less_equal, order="asc")
+            print(" 測 > ")
+            data = list(copy.deepcopy(sample["list"]))
+            self.TestUtil_for1(data=data, test_func=merge1, compare_func=be_bigger, order="desc")
+            print(" 測 >= ")
+            data = list(copy.deepcopy(sample["list"]))
+            self.TestUtil_for1(data=data, test_func=merge1, compare_func=be_bigger_equal, order="desc")
+
+            print("%s 反轉" % (sample["name"],))
+            print(" 測 < ")
+            reversed_data = list(reversed(copy.deepcopy(sample["list"])))
+            self.TestUtil_for1(data=reversed_data, test_func=merge1, compare_func=be_less, order="asc")
+            print(" 測 <= ")
+            reversed_data = list(reversed(copy.deepcopy(sample["list"])))
+            self.TestUtil_for1(data=reversed_data, test_func=merge1, compare_func=be_less_equal, order="asc")
+            print(" 測 > ")
+            reversed_data = list(reversed(copy.deepcopy(sample["list"])))
+            self.TestUtil_for1(data=reversed_data, test_func=merge1, compare_func=be_bigger, order="desc")
+            print(" 測 >= ")
+            reversed_data = list(reversed(copy.deepcopy(sample["list"])))
+            self.TestUtil_for1(data=reversed_data, test_func=merge1, compare_func=be_bigger_equal, order="desc")
+            num += 1
+
+if __name__ == "__main__":
+    unittest.main()
