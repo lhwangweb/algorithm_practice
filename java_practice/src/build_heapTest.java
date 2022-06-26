c93fd0a313d8a794ddc24fe00f02a70444056d8e
hank <lhwang.web@gmail.com>
2022-06-26 22:22:05 +0800

    update print function for heap

--- java_practice/src/build_heapTest.java
+++ java_practice/src/build_heapTest.java
@@ -1,5 +1,4 @@
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,55 +18,59 @@ class build_heapTest {
 
         int[] init_data = new int[] { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 };
         System.out.println(String.format("Origin Heap is: %s", Arrays.toString(init_data)));
-        printHeap(init_data);
+        printFourLevelHeap(init_data);
         build_heap hb = new build_heap(init_data);
         System.out.println(String.format("Max Heap is: %s", Arrays.toString(hb.array_data)));
-        printHeap(hb.array_data);
+        printFourLevelHeap(hb.array_data);
         // 檢查 array
         assertArrayEquals(hb.array_data, new int[] { 17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1 });
 
 
         init_data = new int[] { 1, 3, 5, 4, 6, 13, 10, 8, 15, 17, 9, 27 };
         System.out.println(String.format("Origin Heap is: %s", Arrays.toString(init_data)));
-        printHeap(init_data);
+        printFourLevelHeap(init_data);
         hb = new build_heap(init_data);
         System.out.println(String.format("Max Heap is: %s", Arrays.toString(hb.array_data)));
-        printHeap(hb.array_data);
+        printFourLevelHeap(hb.array_data);
         // 檢查 array
         assertArrayEquals(hb.array_data, new int[] { 27, 17, 13, 15, 9, 5, 10, 8, 4, 6, 3, 1 });
 
         init_data = new int[] { 1, 15, 17, 9, 225, 5, 14, 6, 13, 10, 8, 91, 45};
         System.out.println(String.format("Origin Heap is: %s", Arrays.toString(init_data)));
-        printHeap(init_data);
+        printFourLevelHeap(init_data);
         hb = new build_heap(init_data);
         System.out.println(String.format("Max Heap is: %s", Arrays.toString(hb.array_data)));
-        printHeap(hb.array_data);
+        printFourLevelHeap(hb.array_data);
         // 檢查 array
         assertArrayEquals(hb.array_data, new int[] { 1, 15, 17, 9, 273, 5, 14, 6, 13, 10, 8, 91, 45 });
     }
 
-    static void printHeap(int[] heap_array)
+    /**
+     * 印出 4 層 Heap
+     * @param heap_data
+     */
+    static void printFourLevelHeap(int[] heap_data)
     {
-        int len = heap_array.length;
+        int len = heap_data.length;
         if (len <= 0) {
             return;
         }
 
-        System.out.println(String.format("             %d    ", heap_array[0]));
+        System.out.println(String.format("             %d    ", heap_data[0]));
         System.out.println("       /         \\  ");
-        System.out.println(String.format("      %d            %d", heap_array[1], heap_array[2]));
+        System.out.println(String.format("      %d            %d", heap_data[1], heap_data[2]));
         System.out.println("    /   \\       /    \\");
-        System.out.println(String.format("  %d    %d      %d     %d", heap_array[3], heap_array[4], heap_array[5], heap_array[6]));
+        System.out.println(String.format("  %d    %d      %d     %d", heap_data[3], heap_data[4], heap_data[5], heap_data[6]));
         System.out.println("  / \\   / \\    / \\   / \\");
-        System.out.println(String.format(" %d  %d  %d  %d  %s  %s  %s  %s",
-            heap_array[7],
-            heap_array[8],
-            heap_array[9],
-            heap_array[10],
-            len >= 12 ? heap_array[11]:"x",
-            len >= 13 ? heap_array[12]:"x",
-            len >= 14 ? heap_array[13]:"x",
-            len >= 15 ? heap_array[14]:"x"
+        System.out.println(String.format(" %s  %s  %s  %s  %s  %s  %s  %s",
+            len >= 8 ? heap_data[7]:"x",
+            len >= 9 ? heap_data[8]:"x",
+            len >= 10 ? heap_data[9]:"x",
+            len >= 11 ? heap_data[10]:"x",
+            len >= 12 ? heap_data[11]:"x",
+            len >= 13 ? heap_data[12]:"x",
+            len >= 14 ? heap_data[13]:"x",
+            len >= 15 ? heap_data[14]:"x"
         ));
 
     }
