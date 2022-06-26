54f62c56d9e169228ae2177f4e22391906513ce1
hank <lhwang.web@gmail.com>
2022-06-26 22:20:49 +0800

    新增 heap sort

---
+++ java_practice/src/heap_sort.java
@@ -0,0 +1,210 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  練習 heap 的插入與刪除，以及 heap sort
+ *
+ *  https://www.geeksforgeeks.org/insertion-and-deletion-in-heaps/
+ */
+
+public class heap_sort {
+    ArrayList<Integer> array_data;
+
+    boolean max_heap = true;
+
+    boolean DEBUG = false;
+
+    /**
+     * 建構式
+     * @param data
+     * @param max_heap 堆積類型
+     *    true Max Heap
+     *    false Min Heap
+     */
+    public heap_sort(ArrayList<Integer> data, boolean max_heap)
+    {
+        // 賦予 init data to variable
+        this.array_data = (ArrayList<Integer>) data.clone();  // clone a duplicate to class variable
+        this.max_heap = max_heap;
+
+        // heapify
+        this.heapify();
+
+    }
+
+    /**
+     * 完成 Max/Min Heap 的過程
+     *
+     */
+    public void heapify() {
+        // 進行 build
+        int array_length = this.array_data.size();
+
+        // 從最後一個 element，也就是樹最底層最右邊的 parent
+        int start_index = (array_length - 2) / 2;
+
+        for (int i = start_index; i >= 0; i--) {
+            this.siftDown(i, array_length + 1);
+        }
+    }
+
+    /**
+     * 移除任意一個節點
+     * 是 delete_top_node 的廣義延伸版本
+     * @param i
+     */
+    public void delete_one_node(int i)
+    {
+        int array_length = this.array_data.size();
+
+        // 要刪除的 element ，與最後一個交換
+        swap_element(i, array_length - 1);
+
+        // 為了明確，最後一個 element 設定為 Null 表示刪除 (懶得把 variable 改 arraylist 了)
+        this.array_data.set(array_length - 1, null);
+
+        // sift down No. i node
+        this.siftDown(i, array_length - 1);
+    }
+
+    /**
+     *  練習移除最頂端的一個 node
+     *
+     */
+    public void delete_top_node()
+    {
+        delete_one_node(0);
+    }
+
+    /**
+     *
+     *
+     *  heap sort 跟 build heap 都講得不錯的影片 https://www.youtube.com/watch?v=j-DqQcNPGbE
+     */
+
+    public void heap_sort()
+    {
+        int array_length = this.array_data.size();
+
+        for (int k = array_length - 1; k > 0 ; k--) {
+            if (this.DEBUG == true) {
+                System.out.println(String.format(
+                    "swap %d(0) <-> %s(%d)", this.array_data.get(0), this.array_data.get(k), k
+                ));
+            }
+
+            // 第一個與最後一個交換
+            this.swap_element(0, k);
+
+            if (this.DEBUG == true) {
+                System.out.println(String.format("sift %d", k));
+            }
+
+            // sift down top node
+            this.siftDown(0, k);
+        }
+    }
+
+    // 筆記:
+    // 1. Java 似乎沒辦法直接的給 function argument 預設值
+    // https://stackoverflow.com/questions/997482/does-java-support-default-parameter-values
+    // 2. 類似 keyword argument, named parameter
+    // https://stackoverflow.com/questions/1988016/named-parameter-idiom-in-java
+    //
+    // 怎麼以上兩個事情都有人提 builder pattern ?
+
+    /**
+     *
+     * @param i   要 sift down 的節點 index
+     */
+    public void siftDown(int i, int skip_node_index) {
+        int array_length = this.array_data.size();
+
+        if (i > array_length) {
+            return;
+        }
+
+        int largest_index = i;
+
+        int left_child_index = 2 * i + 1;
+        int right_child_index = 2 * i + 2;
+
+        if (this.max_heap == true) {
+            // 左子節點存在、左子節點比較大、左子節點沒有要跳過
+            if (
+                left_child_index < array_length &&
+                this.array_data.get(left_child_index) > this.array_data.get(largest_index) &&
+                left_child_index < skip_node_index
+            ) {
+                largest_index = left_child_index;
+            }
+
+            // 右子節點存在、右子節點比較大、右子節點沒有要跳過
+            // (注意 條件是跟 largest_index 比，由於上面先比過左子節點，如果左子節點較大，就會替換到 largest_index ，故這裏再比右子節點，就可以完成三者之間最大的放到 parent)
+            if (
+                right_child_index < array_length &&
+                this.array_data.get(right_child_index) > this.array_data.get(largest_index) &&
+                right_child_index < skip_node_index
+            ) {
+                largest_index = right_child_index;
+            }
+        } else {
+            // 左子節點存在、左子節點比較小、左子節點沒有要跳過
+            if (
+                left_child_index < array_length &&
+                this.array_data.get(left_child_index) < this.array_data.get(largest_index) &&
+                left_child_index < skip_node_index
+            ) {
+                largest_index = left_child_index;
+            }
+
+            // 右子節點存在、右子節點比較小、右子節點沒有要跳過
+            if (
+                right_child_index < array_length &&
+                this.array_data.get(right_child_index) < this.array_data.get(largest_index) &&
+                right_child_index < skip_node_index
+            ) {
+                largest_index = right_child_index;
+            }
+        }
+
+        if (largest_index != i) {
+
+            if (this.DEBUG == true) {
+                System.out.println(
+                    String.format("swap %d(idx %d) <-> %d(idx %d)",
+                        this.array_data.get(largest_index),
+                        largest_index,
+                        this.array_data.get(i),
+                        i
+                    )
+                );
+            }
+
+            swap_element(largest_index, i);
+            siftDown(largest_index, skip_node_index);
+        }
+
+    }
+
+    /**
+     * 交換 this.array 裡面的 element
+     *
+     * @param index_a
+     * @param index_b
+     */
+    public void swap_element(int index_a, int index_b) {
+        int temp = this.array_data.get(index_a);
+        this.array_data.set(index_a, this.array_data.get(index_b));
+        this.array_data.set(index_b, temp);
+    }
+
+    public void insert_node(Integer new_element)
+    {
+        // 新 element 加在最後
+        this.array_data.add(new_element);
+
+        // 重新做一輪 heapify
+        this.heapify();
+    }
+}
---
+++ java_practice/src/heap_sortTest.java
@@ -0,0 +1,323 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static java.util.Map.entry;
+import static org.junit.jupiter.api.Assertions.*;
+
+class heap_sortTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void test_sort() {
+        ArrayList<Map<String, Object>> test_cases = new ArrayList();
+
+        // static case
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 1"),
+            entry("data", new ArrayList<Integer>(Arrays.asList(1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17)))
+        ));
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 2"),
+            entry("data", new ArrayList<Integer>(Arrays.asList(1, 3, 5, 4, 6, 13, 32, 8, 55, 16, 10, 9, 8, 15, 17, 91, 2)))
+        ));
+
+        // random case
+        Random rd = new Random();
+        Integer [] this_array;
+        int random_length;
+        ArrayList<Integer> this_case;
+
+        // case 1
+        this_case = new ArrayList();
+        random_length = 18; // 長度 18
+        for (int i = 0; i < random_length; i++) {
+            this_case.add(rd.nextInt(100) + 1);
+        }
+        Collections.shuffle(this_case);
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 4，長度 18，隨機"),
+            entry("data", this_case)
+        ));
+
+        // case 2
+        this_case = new ArrayList();
+        random_length = rd.nextInt(30) + 7; // 7~36
+        for (int i = 0; i < random_length; i++) {
+            this_case.add(rd.nextInt(100) + 1); // element 1 ~100
+        }
+        Collections.shuffle(this_case);
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 5，長度 7~36，隨機"),
+            entry("data", this_case)
+        ));
+
+        // case 3
+        this_case = new ArrayList();
+        random_length = rd.nextInt(20) + 7; // 7~26
+        for (int i = 0; i < random_length; i++) {
+            this_case.add(rd.nextInt(100) + 1); // element 1 ~100
+        }
+        this_case.add(30); this_case.add(30); this_case.add(30); this_case.add(30); this_case.add(30);
+        Collections.shuffle(this_case);
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 6，長度 7~26，隨機，加入相同元素"),
+            entry("data", this_case)
+        ));
+
+        // case 4
+        this_case = new ArrayList();
+        random_length = rd.nextInt(18) + 8; // 8~25
+        for (int i = 0; i < random_length; i++) {
+            this_case.add(rd.nextInt(75) + 1); // element 1 ~100
+        }
+        this_case.add(60); this_case.add(60);
+        this_case.add(25); this_case.add(25); this_case.add(25);
+        this_case.add(17); this_case.add(17); this_case.add(17); this_case.add(17); this_case.add(17);
+        Collections.shuffle(this_case);
+        test_cases.add(Map.ofEntries(
+            entry("name", "範例 7，長度 8~25，隨機，加入相同元素"),
+            entry("data", this_case)
+        ));
+
+        // 開始測試
+        heap_sort hb_instance;
+        ArrayList<Integer> test_data;
+        ArrayList<Integer> answer_data;
+
+        // 跑測試
+        for (Map entry : test_cases) {
+            try {
+                System.out.println(entry.get("name"));
+                test_data = (ArrayList<Integer>) entry.get("data");
+                answer_data =  (ArrayList<Integer>) test_data.clone();
+
+                // max heap and asc
+                hb_instance = new heap_sort(test_data, true);
+                hb_instance.heap_sort();
+                System.out.println(String.format(
+                    "ASC: %s -> %s", Arrays.toString(test_data.toArray()), Arrays.toString(hb_instance.array_data.toArray())
+                ));
+
+                // asc sort answer and assert
+                Collections.sort(answer_data);
+                assertArrayEquals(hb_instance.array_data.toArray(), answer_data.toArray());
+
+                // min heap and desc
+                hb_instance = new heap_sort(test_data, false);
+                hb_instance.heap_sort();
+                System.out.println(String.format(
+                    "DESC: %s -> %s", Arrays.toString(test_data.toArray()), Arrays.toString(hb_instance.array_data.toArray())
+                ));
+
+                // desc sort answer and assert
+                Collections.sort(answer_data, Collections.reverseOrder());
+                assertArrayEquals(hb_instance.array_data.toArray(), answer_data.toArray());
+
+            } catch (Exception eee) {
+                System.out.println(eee);
+            }
+
+        }
+    }
+
+    @Test
+    public void test_deletion() {
+        ArrayList<Integer> test_case = new ArrayList<Integer>(
+                Arrays.asList(1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17)
+        );
+
+        // remove case 1 - top node
+        heap_sort hb_instance = new heap_sort(test_case, true);
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        hb_instance.delete_top_node();
+
+        System.out.println("case 1 (remove top node) after");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        // remove case 2 - any node
+        hb_instance = new heap_sort(test_case, true); // 重新 init
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        hb_instance.delete_one_node(3);
+
+        System.out.println("case 2 (remove 3 node, value 9) (17, 15, 13, 9 ...)after");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        // remove case 3 - any node
+        hb_instance = new heap_sort(test_case, true); // 重新 init
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+        hb_instance.delete_one_node(7);
+
+        System.out.println("case 3 (remove 7 node, value 4) (17, 15, 13, 9, 6, 5, 10, 4) after");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        // remove case 4 - any node
+        hb_instance = new heap_sort(test_case, true); // 重新 init
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+        hb_instance.delete_one_node(2);
+
+        System.out.println("case 4 (remove 2 node, value 13) (17, 15, 13, ...) after");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        // remove case 4 - any node
+        hb_instance = new heap_sort(test_case, true); // 重新 init
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+        hb_instance.delete_one_node(1);
+
+        System.out.println("case 5 (remove 1 node, value 15) (17, 15, 13, ...) after");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+    }
+
+    @Test
+    public void test_insertion() {
+        ArrayList<Integer> test_case = new ArrayList<Integer>(
+            Arrays.asList(1, 3, 4, 6, 13, 10, 9, 15, 17)
+        );
+
+        // insert case 1
+        heap_sort hb_instance = new heap_sort(test_case, true);
+        System.out.println("before");
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println("case 1, insert 40");
+        hb_instance.insert_node(40);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 2, insert 14 (還沒有的 element)");
+        hb_instance.insert_node(14);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 3, insert 15 (有相同的 element)");
+        hb_instance.insert_node(15);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 4, insert 15 (有相同的 element)");
+        hb_instance.insert_node(15);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 5, insert 15 (有相同的 element)");
+        hb_instance.insert_node(15);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println(" re-init heap ");
+
+        hb_instance = new heap_sort(test_case, true);
+        System.out.println("case 1, insert 15 (有相同的 element)");
+        hb_instance.insert_node(15);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 2, insert 15 (有相同的 element)");
+        hb_instance.insert_node(15);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 3, insert 17 (有相同的 element)");
+        hb_instance.insert_node(17);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 4, insert 17 (有相同的 element)");
+        hb_instance.insert_node(17);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+        System.out.println("case 5, insert 36 (有相同的 element)");
+        hb_instance.insert_node(36);
+        System.out.println(Arrays.toString(hb_instance.array_data.toArray(new Integer[0])));
+        printFourLevelHeap(hb_instance.array_data);
+
+        System.out.println(" -- ");
+
+    }
+
+    /**
+     * 印出 4 層 Heap
+     * @param heap_data
+     */
+    static void printFourLevelHeap(ArrayList<Integer> heap_data)
+    {
+        int len = heap_data.size();
+        if (len <= 0) {
+            return;
+        }
+
+        System.out.println(String.format("             %d    ", heap_data.get(0)));
+        System.out.println("       /         \\  ");
+        System.out.println(String.format("      %d            %d", heap_data.get(1), heap_data.get(2)));
+        System.out.println("    /   \\       /    \\");
+        System.out.println(String.format("  %d    %d      %d     %d", heap_data.get(3), heap_data.get(4), heap_data.get(5), heap_data.get(6)));
+        System.out.println("  / \\   / \\    / \\   / \\");
+        System.out.println(String.format(" %s  %s  %s  %s  %s  %s  %s  %s",
+            len >= 8 ? heap_data.get(7):"x",
+            len >= 9 ? heap_data.get(8):"x",
+            len >= 10 ? heap_data.get(9):"x",
+            len >= 11 ? heap_data.get(10):"x",
+            len >= 12 ? heap_data.get(11):"x",
+            len >= 13 ? heap_data.get(12):"x",
+            len >= 14 ? heap_data.get(13):"x",
+            len >= 15 ? heap_data.get(14):"x"
+        ));
+
+    }
+}
