04bc29bbdb9773ff3437bdda862edbf95fa571bf
hank <lhwang.web@gmail.com>
2022-06-26 22:19:51 +0800

    update poc in hello world

--- java_practice/src/hello_world.java
+++ java_practice/src/hello_world.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class hello_world {
     public static void main(String[] args) {
@@ -16,6 +17,19 @@ public class hello_world {
         System.out.println(b);
         System.out.println(Arrays.toString(b));
 
+        int[] c = new int [] {1,2,7,92,16,53,31,24,78,17,82,5,16};
+        int[] d;
+        d = c;
+        Arrays.sort(d);
+        System.out.println(c);
+        System.out.println(Arrays.toString(c));
+        System.out.println(d);
+        System.out.println(Arrays.toString(d));
+
+        // primitive arrays 想要 DESC
+        // Arrays.sort() cannot be used directly to sort primitive arrays in descending order
+        // That will work fine with 'Array of Objects' such as Integer array but will not work with a primitive array such as int array.
+        // https://stackoverflow.com/questions/1694751/java-array-sort-descending
 
 
     }
