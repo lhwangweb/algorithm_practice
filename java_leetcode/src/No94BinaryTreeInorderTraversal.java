30a7df2eb0d44aa44d61278e3c91922bb62e2ac2
hank <lhwang.web@gmail.com>
2022-07-09 20:31:54 +0800

    LeetCode No.94 In Order Traversal

---
+++ java_leetcode/src/No94BinaryTreeInorderTraversal.java
@@ -0,0 +1,95 @@
+/**
+ * 94. Binary Tree Inorder Traversal
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ *
+ * Runtime: 1 ms, faster than 48.73% of Java online submissions for Binary Tree Inorder Traversal.
+ * Memory Usage: 42.3 MB, less than 49.14% of Java online submissions for Binary Tree Inorder Traversal.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class No94BinaryTreeInorderTraversal {
+    public static void Main(String[] args) {
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+
+class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
+}
+
+class Solution {
+    /**
+     * 以 Stack + while loop 完成 in order traversal
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> traversal_result = new ArrayList<Integer>() {};
+        Stack<TreeNode> traversal_stack = new Stack<TreeNode>();
+
+        // root null 就可以 return [] 了
+        if (root == null) {
+            return traversal_result;
+        }
+
+        TreeNode tmp_clone_node = null;
+        TreeNode work_node = root;
+
+        while (true) {
+
+            // 1. 是否有左子節點？
+            if (work_node.left != null) {
+                // 1.1 有左子節點
+
+                // 1.1.1 把自己(根節點)丟進 Stack，但在丟進去要把 左子節點刪除，以免之後從 Stack 取出時，又會重複走訪左子節點
+                tmp_clone_node = new TreeNode(work_node.val, null, work_node.right); // clone 並使左子節點 null
+                traversal_stack.push(tmp_clone_node); // 加入 stack
+
+                // 1.1.2 往左走
+                work_node = work_node.left;
+                continue;
+            }
+
+            // 2. 走到這裡的節點，應該都沒有左子節點
+
+            // 3. value 加入 ArrayList (此節點可能是左 leaf or 根，都是在這個環節加入 ArrayList)
+            traversal_result.add(work_node.val);
+
+            // 4. 是否有右子節點？
+            if (work_node.right != null) {
+                // 4.1 有右子節點
+
+                // 4.1.1 向右走
+                work_node = work_node.right;
+                continue;
+            }
+
+            // 5. 準備從 Stack 取出下一個
+            if (traversal_stack.isEmpty()) {
+                // 5.1 若 Stack 空了就結束
+                break;
+            }
+
+            // 5.2 取出節點
+            work_node = traversal_stack.pop();
+        }
+
+        return traversal_result;
+    }
+}
+
---
+++ java_leetcode/src/No94BinaryTreeInorderTraversalTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class No94BinaryTreeInorderTraversalTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test_main() {
+        Solution test_solution = new Solution();
+        List<Integer> res;
+
+        TreeNode c = new TreeNode(3);
+        TreeNode b = new TreeNode(2, c, null);
+        TreeNode a = new TreeNode(1, null, b);
+        //   1
+        //    \
+        //     2
+        //    /
+        //   3
+
+        res = test_solution.inorderTraversal(a);
+        System.out.println(Arrays.toString(res.toArray()));
+        assertArrayEquals(res.toArray(), new Integer[] {1, 3, 2});
+
+        TreeNode a3 = new TreeNode(3);
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a4 = new TreeNode(4, a3, null );
+        TreeNode a2 = new TreeNode(2, a1, a4);
+
+        //   2
+        //  / \
+        // 1   4
+        //    /
+        //   3
+
+        res = test_solution.inorderTraversal(a2);
+        System.out.println(Arrays.toString(res.toArray()));
+        assertArrayEquals(res.toArray(), new Integer[] {1, 2, 3, 4});
+
+        TreeNode b3 = new TreeNode(3);
+        TreeNode b7 = new TreeNode(7);
+        TreeNode b8 = new TreeNode(8);
+        TreeNode b5 = new TreeNode(5, b3, b7);
+        TreeNode b4 = new TreeNode(4, b8, null);
+        TreeNode b2 = new TreeNode(2, b5, b4);
+
+        //      2
+        //    /   \
+        //   5     4
+        //  / \   /
+        // 3   7 8
+
+        res = test_solution.inorderTraversal(b2);
+        System.out.println(Arrays.toString(res.toArray()));
+        assertArrayEquals(res.toArray(), new Integer[] {3,5,7,2,8,4});
+    }
+}
