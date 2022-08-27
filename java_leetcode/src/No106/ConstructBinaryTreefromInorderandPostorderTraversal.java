6e46379f817c52c5d889f11d225bf404e38e5cd2
hank <lhwang.web@gmail.com>
2022-08-27 16:09:32 +0800

    改進 No 106 (第三次)

--- java_leetcode/src/No106/ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ java_leetcode/src/No106/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -3,14 +3,26 @@
  * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
  *
  * 第一次
- * Runtime: 371 ms, faster than 5.15% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
- * Memory Usage: 117.4 MB, less than 5.04% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+ *  結果
+ *   Runtime: 371 ms, faster than 5.15% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+ *   Memory Usage: 117.4 MB, less than 5.04% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
  *
- * 第二次 調整，至少 map 的部分不要做 argument 一直遞迴，可能是慢速＆多耗空間的兇手
- * Runtime: 342 ms, faster than 5.15% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
- * Memory Usage: 117.2 MB, less than 5.04% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
- * 沒有改善，不是遞迴傳 Map 的問題？ (也對，這是 mutable 的，傳遞進去不會增加 Stack )
+ * 第二次
+ *  調整
+ *    嘗試至少 map 的部分不要做 argument 一直遞迴，可能是慢速＆多耗空間的兇手
+ *  結果
+ *   Runtime: 342 ms, faster than 5.15% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+ *   Memory Usage: 117.2 MB, less than 5.04% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
  *
+ *   沒有改善，不是遞迴傳 Map 的問題？ (也對，這是 mutable 的，傳遞進去不會增加 Stack )
+ *
+ * 第三次
+ *  調整
+ *   修改演算法，運用 preorder in order 的特性，遞迴的向子樹方向拆解，等遞迴結束條件達成時，即代表拆解到 leaf，之後開始 return 時即開始建構樹
+ *   依據寫 preorder 的經驗，不使用過度冗余的 if 或 變數
+ *  結果
+ *   Runtime: 3 ms, faster than 87.50% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+ *   Memory Usage: 44.5 MB, less than 41.68% of Java online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
  */
 
 package No106;
@@ -50,6 +62,10 @@ class Solution {
      */
     Map<Integer, Integer> inorder_index_map = new HashMap<Integer, Integer>();
 
+    int[] postorder;
+
+    int[] inorder;
+    
     /**
      * 新增一個 Node - 嘗試把新節點要放到目前節點的左或右子節點
      * 要丟到左還是右，依據兩個節點的值在 In Order 內的相對位置判斷
@@ -142,12 +158,73 @@ class Solution {
      *
      *  由於原本想的方法經 Leetcode submission 檢驗，很慢，參考了 Leetcode 較快的 Solution，練習一次
      *
-     * @param preorder
+     * @param postorder
      * @param inorder
      * @return
      */
-    public TreeNode buildTree_v2(int[] preorder, int[] inorder) {
+    public TreeNode buildTree_v2(int[] inorder, int[] postorder) {
+        // 有任一 array 為空 or 長度不符
+        if (postorder.length <= 0 || inorder.length <= 0 || postorder.length != inorder.length) {
+            return null;
+        }
 
+        // 填充 map
+        for (int i = 0; i < inorder.length; i++) {
+            this.inorder_index_map.put(inorder[i], i);
+        }
+
+        // 放到
+        this.inorder = inorder;
+        this.postorder = postorder;
+
+        TreeNode node = build_tree(0, postorder.length-1, 0, inorder.length - 1);
+
+        return node;
+    }
+
+
+    /**
+     *
+     * @param postorder_start postorder 開始
+     * @param postorder_end   postorder 結束
+     * @param inorder_start
+     * @param inorder_end
+     * @return
+     */
+    public TreeNode build_tree(int postorder_start, int postorder_end, int inorder_start, int inorder_end) {
+        if (postorder_start > postorder_end || inorder_start > inorder_end || postorder_start < 0 || postorder_end < 0 || inorder_start < 0 || inorder_end < 0) {
+            // 各個起訖 index 不可能出現 開始比結束大也不可能為負數。
+            return null;
+        }
+
+        // postorder 的最後一個，就是這棵樹的頂點
+        int this_node_value = this.postorder[postorder_end]; // Node Value
+        TreeNode this_node = new TreeNode(this_node_value);
+
+        if (postorder_start == postorder_end) {
+            // 此時只有 1 個 element，故直接 return
+            return this_node;
+        }
+
+        int index_in_inorder = this.inorder_index_map.get(this_node_value); // 此 Value 在 inorder 內的 Index
+
+        // inorder array 分布：  [ ...左子樹... ][root][ ...右子樹... ]
+        // inorder 的左子樹
+        int left_inorder_start = inorder_start; // inorder 的左子樹起點
+        int left_inorder_end = index_in_inorder - 1; // inorder 的左子樹終點
+        int left_length = index_in_inorder - inorder_start; // 左子樹長度
+
+        // postorder array 分布：  [ ...左子樹... ][ ...右子樹... ][root]
+        int left_postorder_start = postorder_start; // postorder 中的左子樹起點
+        int left_postorder_end = postorder_start + left_length - 1;  // postorder 中的左子樹的終點， 用上面 inorder 的左子樹長度推得  '起點index' + 'length' - 1 = '終點index'
+
+        // 遞迴建構左子樹 (使用上面計算出來的 左子樹 的 起迄 index)
+        this_node.left = build_tree(left_postorder_start, left_postorder_end, left_inorder_start, left_inorder_end);
+
+        // 遞迴建構右子樹 (右子樹 的 起迄 index，使用上面計算出來的相關 index 去得到)
+        this_node.right = build_tree(left_postorder_end + 1, postorder_end - 1, index_in_inorder + 1, inorder_end);
+
+        return this_node;
     }
 
     /**
