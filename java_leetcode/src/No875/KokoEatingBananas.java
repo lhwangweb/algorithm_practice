97790d7d99d1db0330c69b7dae4a9c2a7e9c12d3
hank <lhwang.web@gmail.com>
2022-10-25 20:38:31 +0800

    No 875 第九次 嘗試不要猜 init 值

--- java_leetcode/src/No875/KokoEatingBananas.java
+++ java_leetcode/src/No875/KokoEatingBananas.java
@@ -43,9 +43,17 @@ import java.util.Collections;
  *  檢討： 除法 vs 乘法＋int轉型 比較下，除法還是比較慢
  *
  *  第八次 參考了他人的寫法，比較漂亮一點的 binary search 寫法
+ *     Runtime: 27 ms, faster than 66.54% of Java online submissions for Koko Eating Bananas.
+ *     Memory Usage: 54.5 MB, less than 15.43% of Java online submissions for Koko Eating Bananas.
  *
- *  Runtime: 27 ms, faster than 66.54% of Java online submissions for Koko Eating Bananas.
- *  Memory Usage: 54.5 MB, less than 15.43% of Java online submissions for Koko Eating Bananas.
+ *  檢討： 誒？ 竟然沒有比較快
+ *
+ *  第九次 移除 init 的部分，看看是否會改善
+ *     Runtime: 28 ms, faster than 65.07% of Java online submissions for Koko Eating Bananas.
+ *     Memory Usage: 53.9 MB, less than 47.51% of Java online submissions for Koko Eating Bananas.
+ *
+ *  檢討：還是有稍微變慢
+ *
  *
  */
 public class KokoEatingBananas {
@@ -64,24 +72,10 @@ class Solution {
         // Constraints: 1 <= piles.length <= 10000, 所以不擔心 empty array
         // Constraints: piles.length <= h <= 10^9, 所以不用擔心 piles.length > h 這種違反題目宗旨的情境
         // 給定條件 length <= h
-
-        int max_banana_pile = -1;  // 最多香蕉數
-        long sum_banana_piles = 0; // 香蕉總和
-        for (int pile: piles) {
-            sum_banana_piles += pile;
-            if (max_banana_pile < pile) {
-                max_banana_pile = pile;
-            }
-        }
-
-        // 總時數
+        
         long total_cost_hour = 0; // Constraints: 每個 piles 最大可能到 10^9 ，相關 hour 也可能到 10^9 ，所以要用 long 才足夠計算，否則會破表
-
-        int speedK_upper_limit = max_banana_pile; // 速度最大值（上限)
-        int speedK_lower_limit = (int)(sum_banana_piles/h); // 速度最小值（下限）
-        if (speedK_lower_limit<1) {speedK_lower_limit=1;}
-
-        // 迭代用的當下速度
+        int speedK_upper_limit = 1000000000;
+        int speedK_lower_limit = 1;
         int speedK = 1;
 
         // 迴圈去猜總時數
